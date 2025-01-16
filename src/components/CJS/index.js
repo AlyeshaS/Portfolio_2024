fad899fdc58f3c9ef02a3774c0c08909ce4fde49
AlyeshaS <alyeshasingh04@gmail.com>
2025-01-16 00:55:06 -0500

    CJS page added

--- src/components/CJS/index.js
+++ src/components/CJS/index.js
@@ -8,6 +8,13 @@ import "swiper/css/effect-flip";
 import "swiper/css/pagination";
 
 import CJSFigma from "../../assets/CJSFigma.png";
+import CJSAbout from "../../assets/CJSAbout.png";
+import CJSAbout2 from "../../assets/CJSAbout2.png";
+import CJSAchievements from "../../assets/CJSAchievements.png";
+import CJSAchievements2 from "../../assets/CJSAchievements2.png";
+import CJSContact from "../../assets/CJSContact.png";
+import CJSTestimonials from "../../assets/CJSTestimonials.png";
+import CJSServices from "../../assets/CJSServices.png";
 
 // Fix the mobile image and then also check the side align for the second image
 function CJS() {
@@ -31,6 +38,14 @@ function CJS() {
           <p className="CJS-header">CJS</p>
           <p>Freelance UI/UX Designer</p>
           <p>August 2024 - October 2024</p>
+          <a
+            href="https://cjsperformance.online/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="view-website-link"
+          >
+            <p>View Official Website</p>
+          </a>
         </div>
         <div className="swipper-container">
           <Swiper
@@ -48,16 +63,26 @@ function CJS() {
             <SwiperSlide className="slide">
               <div className="text">
                 <div>
-                  <h1 className="CJS-page-titles">Dashboard (Old) </h1>
+                  <h1 className="CJS-page-titles">Home</h1>
                 </div>
                 <div className="text-scroll">
                   <p>
-                    CJS is a website designed for a soccer coach looking to
-                    connect with players of all ages seeking professional
-                    training. The platform serves as a tool to attract and
-                    generate leads for new students, helping them improve their
-                    skills through tailored coaching sessions.
+                    Since the purpose of the website was to gain clients, I
+                    choose to have it be a the coach and his students with a
+                    train now button. The image and the bold button gives
+                    professionalism of coaching and entises them to see what
+                    this coach has to offer.
                   </p>
+                  <br />
+                  <p>
+                    I choose to have green as the accent colour as it signified
+                    health, which was discovered upon some research. It also
+                    allows us to highlight important information/buttons where
+                    needed. The contact us button at the top right of the screen
+                    turn green upon hover, which is a nice touch as well.
+                  </p>
+                  <br />
+                  <p>The about me page has a similar layout.</p>
                 </div>
               </div>
               <div className="content">
@@ -67,20 +92,104 @@ function CJS() {
             <SwiperSlide className="slide">
               <div className="text">
                 <div>
-                  <h1 className="CJS-page-titles">Dashboard (Old) </h1>
+                  <h1 className="CJS-page-titles">About </h1>
                 </div>
                 <div className="text-scroll">
                   <p>
-                    CJS is a website designed for a soccer coach looking to
-                    connect with players of all ages seeking professional
-                    training. The platform serves as a tool to attract and
-                    generate leads for new students, helping them improve their
-                    skills through tailored coaching sessions.
+                    There were two parts talking about the coach. It was
+                    important to have a brief description of the coach on the
+                    main page, for those who just want to get an understanding.
+                    They have the ability to watch a video (provided by my
+                    client) and/or click the read more button for more.
                   </p>
                 </div>
               </div>
               <div className="CJS-image">
-                <img src={CJSFigma} alt="" />
+                <img src={CJSAbout} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="slide">
+              <div className="text">
+                <div>
+                  <h1 className="CJS-page-titles">Achievements </h1>
+                </div>
+                <div className="text-scroll">
+                  <p>
+                    Another important aspect when dealing with a website to gain
+                    clients, is to talk about why them. This section highlights
+                    3 key aspects that they can provide if you choose them.
+                    Since he is a soccer coach, I looked for background images
+                    relating to the sport. Once again, the green is used again
+                    to highlight the title and having a simple icon to represent
+                    the information on that card.
+                  </p>
+                </div>
+              </div>
+              <div className="CJS-image">
+                <img src={CJSAchievements} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="slide">
+              <div className="text">
+                <div>
+                  <h1 className="CJS-page-titles">Testimonials</h1>
+                </div>
+                <div className="text-scroll">
+                  <p>
+                    My client wanted to have a testimonial section highlighting
+                    words from his past students. This section I decided to have
+                    a horizontal slider which allowed the easy display of the
+                    kind words from these individuals. Additionally, if the
+                    testimonals were lengthy, the content would be scrollable
+                    vertically. This allows consistency of the card dimensions
+                    while showcases whatever is needed.
+                  </p>
+                </div>
+              </div>
+              <div className="CJS-image">
+                <img src={CJSTestimonials} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="slide">
+              <div className="text">
+                <div>
+                  <h1 className="CJS-page-titles">Contact Us</h1>
+                </div>
+                <div className="text-scroll">
+                  <p>
+                    As this website is to gain clients, another aspect that is
+                    good to have, is a contact us section. I choose the
+                    background image to be him as this website is to choose him
+                    as your training coach. In this section, the addition of
+                    location, phone number and email are easily accessible. The
+                    green is used to seperate the two parts in this card as well
+                    as highlight the submit button. This section is added to
+                    every page.
+                  </p>
+                </div>
+              </div>
+              <div className="CJS-image">
+                <img src={CJSContact} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="slide">
+              <div className="text">
+                <div>
+                  <h1 className="CJS-page-titles">Services</h1>
+                </div>
+                <div className="text-scroll">
+                  <p>
+                    The coach has different training options. The best way I
+                    thought of to display this information is to have dropdowns
+                    with an image for the headers. These images are related to
+                    soccer as he is a soccer coach. The dropdowns include the
+                    information provided. Since contacting him is an important
+                    aspect, the contact us section is added to every page.
+                  </p>
+                </div>
+              </div>
+              <div className="CJS-image">
+                <img src={CJSServices} alt="" />
               </div>
             </SwiperSlide>
           </Swiper>
@@ -119,7 +228,7 @@ function CJS() {
         </div>
 
         <div className="CJS-analysis">
-          <img src={CJSFigma} alt="" />
+          <img src={CJSAbout2} alt="" />
           <div className="analysis-div">
             <p className="analysis-header">Design Process</p>
             <p className="analysis-text">
@@ -181,7 +290,7 @@ function CJS() {
           </div>
           {/* Fix the size of the mobile version */}
           <div className="CJS-image">
-            <img src={CJSFigma} alt="" />
+            <img src={CJSAchievements2} alt="" />
           </div>
         </div>
       </div>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -140,9 +140,9 @@ function Navbar() {
               {dropdownOpen && (
                 <div className="dropdown-menu">
                   <a href="/case-studies/ao">Academic Oasis</a>
-                  {/* <a href="/case-studies/cheffery">Cheffery</a>
+                  {/* <a href="/case-studies/cheffery">Cheffery</a> */}
                   <a href="/case-studies/cjs">CJS</a>
-                  <a href="/case-studies/fitcheck">FitCheck</a> */}
+                  {/* <a href="/case-studies/fitcheck">FitCheck</a> */}
                 </div>
               )}
             </div>
